cdc502cb7eac2ea9d41af95cafb3ad8e7cf79087
Johongir10 <shavkatovj73@gmail.com>
2024-07-08 10:54:54 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,8 @@
+let ism=prompt("Ism kiriting");
+
+let harf=prompt(" Hohlagan  bitta  Harf  kiriting");  
+ if(ism.includes(harf)){
+    alert(ism + " ismida " +harf +"  mavjud" )
+ }else{
+    alert(ism + " ismida " +harf +" mavjud   emas")
+ }
